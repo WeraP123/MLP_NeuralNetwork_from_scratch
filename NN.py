0394ded1e27c4c3d198bdbbb6653b5c2fbf069fa
WeraP123 <56978305+WeraP123@users.noreply.github.com>
2022-12-09 07:32:48 +0000

    Add files via upload

---
+++ NN.py
@@ -0,0 +1,14 @@
+class NeuralNetHolder:
+
+    def __init__(self):
+        super().__init__()
+
+        #create network topology using python files
+        # load previously saved weights into the network
+
+    def predict(self, input_row):
+        # make prediction
+#preprocess values
+# pas then into the networrk
+# denormalise
+        return vel_x, vel_y
--- model_nn.py
+++ model_nn.py
@@ -5,13 +5,13 @@ from sklearn.model_selection import train_test_split
 
 
 
-def preprocessing_split(data):
+def preprocessing_split(data, split):
     data = (data - data.min()) / (data.max() - data.min())
     x = data[['x','y']]
     y = data[['vel_x','vel_y']]
     x = x.to_numpy()
     y = y.to_numpy()
-    X_train, X_val, y_train, y_val = train_test_split(x, y, test_size=0.33, random_state=12)
+    X_train, X_val, y_train, y_val = train_test_split(x, y, test_size= split, random_state=12)
     return X_train, X_val, y_train, y_val
 
 
@@ -31,6 +31,9 @@ class NN:
         self.iteration = 0
         self.bias1 = np.random.randn(1, n_neuron)
         self.bias2 = np.random.randn(1, 1)
+        self.temp_delta = []
+        self.temp_delta_hid = []
+
     # W2 = np.random.rand(2, n_neuron)
 
     def sigmoid(self, v):
@@ -54,34 +57,24 @@ class NN:
         rmse_err = []
         errors = []
 
-        while True:
-            for i in range(len(self.inputs)):
-                self.inputs = self.inputs.T
+        while epoch_counter < 80:
+            for i in range(self.inputs.shape[0]):
+
                 self.x = self.inputs[i]
                 self.y = self.outputs[i].T
                 self.feedforward()
                 self.backpropagation()
                 errors.append(self.error_signal)
-
                 self.iteration = self.iteration + 1
-            epoch_counter = epoch_counter + 1
 
+
+            epoch_counter = epoch_counter + 1
             mse = np.square(errors).mean()
             rmse = math.sqrt(mse)
             rmse_err.append(rmse)
 
-            #if epoch_counter >4:
-            #    if rmse_err[epoch_counter-1] > rmse_err[epoch_counter - 2]:
-            #        print("Early", rmse_err[epoch_counter-1])
-            #        break
-            #    else:
-            #        continue
-            if epoch_counter == 2000:
-                print("200 epochs", rmse_err[-1])
-                break
-
-
-
+        np.savetxt('W2.txt', self.W2)
+        np.savetxt('W1.txt', self.W1)
         plt.plot(range(0,epoch_counter), rmse_err)
         plt.xlabel('Epoch')
         plt.ylabel('Error')
@@ -99,10 +92,19 @@ class NN:
         h_grad = self.learning_rate * (self.derivative_sigmoid(self.Z) * np.dot(self.error_signal, self.W2))
         delta_hidden = np.dot(np.reshape(self.x, (2,1)), h_grad)
 
+        self.temp_delta.append(delta_weight.T)
+        self.temp_delta_hid.append(delta_hidden)
+
+
         self.bias1 = self.bias1 + sum(delta_hidden)
         self.bias2 = self.bias2 + sum(delta_weight)
-        self.W2 = delta_weight.T + self.W2
-        self.W1 = delta_hidden + self.W1
+
+        if self.iteration == 0:
+            self.W2 = np.reshape(delta_weight, (2, self.n_neuron)) + self.W2
+            self.W1 = np.reshape(delta_hidden, (2, self.n_neuron)) + self.W1
+        else:
+            self.W2 = np.reshape(delta_weight, (2, self.n_neuron)) + self.W2 + self.momentum_rate * np.array(self.temp_delta[-1])
+            self.W1 = np.reshape(delta_hidden, (2, self.n_neuron)) + self.W1 + self.momentum_rate * np.array(self.temp_delta_hid[-1])
 
     def predict(self, new_input):
         pred = self.feedforward()
